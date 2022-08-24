53ced71d03b71abbf62c7f22b55cdad983773df9
Christopher Hill <cjameshill@gmail.com>
2022-08-24 12:28:58 +0000

    Updated script code for local dev

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,13 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+})
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
   </div>
 </template>
 
