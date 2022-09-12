90e3fa9afe54d4b38204e448bf1d861bc31d6259
Christopher Hill <cjameshill@gmail.com>
2022-09-12 17:21:28 +0000

    Added websocket demo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TheWelcome from '@/components/TheWelcome.vue'
 import { useWebSocket } from '@vueuse/core'
-const { status, data, send, open, close } = useWebSocket('ws://websocket')
+const { status, data, send, open, close } = useWebSocket('wss://devproxy.staracademyapp.com/_t-ws-demo?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self')
 const sendMessage = () => {
   send(JSON.stringify({ message: "message"}))
 }
@@ -12,8 +12,8 @@ const closeConnection = () => {
 
 <template>
   <main>
-    <button @click="closeConnection">close</button>
-    <button @click="sendMessage">send</button>
+    <button classs="p-4 bg-gray-100" @click="closeConnection">close</button>
+    <button classs="p-4 bg-gray-100" @click="sendMessage">send</button>
     <pre>
           Status: {{ status }}
           Data: {{ data }}
