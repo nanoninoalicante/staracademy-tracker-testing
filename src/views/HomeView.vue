53f9b4f0140ddf79813a5d153c98f5a450aa48a0
Christopher Hill <cjameshill@gmail.com>
2022-09-12 17:10:08 +0000

    Added websocket demo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
 import TheWelcome from '@/components/TheWelcome.vue'
+import { useWebSocket } from '@vueuse/core'
+const { status, data, send, open, close } = useWebSocket('ws://websocket')
+const sendMessage = () => {
+  send(JSON.stringify({ message: "message"}))
+}
+const closeConnection = () => {
+  close();
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <button @click="closeConnection">close</button>
+    <button @click="sendMessage">send</button>
+    <pre>
+          Status: {{ status }}
+          Data: {{ data }}
+      </pre>
   </main>
 </template>
