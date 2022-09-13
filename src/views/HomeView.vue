40bd6ede677d20586a66210c9f8ec288ecbdcadf
Christopher Hill <cjameshill@gmail.com>
2022-09-13 14:41:52 +0000

    Added proxy ws, open and send message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,28 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
 import { useWebSocket } from '@vueuse/core'
-const { status, data, send, open, close } = useWebSocket('wss://devproxy.staracademyapp.com/_t-ws-demo?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self')
+import random from "random-words";
+const { status, data, send, open, close } = useWebSocket('wss://0yif8r8ode.execute-api.us-east-1.amazonaws.com/staging')
 const sendMessage = () => {
-  send(JSON.stringify({ message: "message"}))
+  send(JSON.stringify({ message: random({ exactly: 2, join: '-' }), ts: new Date().toISOString() }))
 }
 const closeConnection = () => {
   close();
+}
+const openConnection = () => {
+  open();
 }
 </script>
 
 <template>
   <main>
-    <button classs="p-4 bg-gray-100" @click="closeConnection">close</button>
-    <button classs="p-4 bg-gray-100" @click="sendMessage">send</button>
-    <pre>
-          Status: {{ status }}
-          Data: {{ data }}
-      </pre>
+    <button class="p-4 bg-gray-100 m-2 hover:bg-gray-300" @click="openConnection">open</button>
+    <button class="p-4 bg-gray-100 m-2 hover:bg-gray-300" @click="closeConnection">close</button>
+    <button class="p-4 bg-gray-100 m-2 hover:bg-gray-300" @click="sendMessage">send</button>
+    <p class="break-normal">
+      Status: {{ status }}
+    </p>
+    <p class="break-normal">
+      Data: {{ data }}
+    </p>
   </main>
 </template>
