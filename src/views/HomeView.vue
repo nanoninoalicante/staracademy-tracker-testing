55bd452c108feda1aed45726efe2f871ef1c0f4f
Christopher Hill <cjameshill@gmail.com>
2023-03-09 13:41:33 +0000

    Update 4 files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,49 @@
 <script setup>
-import { useWebSocket } from '@vueuse/core'
+import { useWebSocket } from "@vueuse/core";
 import random from "random-words";
-const { status, data, send, open, close } = useWebSocket('wss://devproxy.staracademyapp.com/_t-ws-demo')
+const { status, data, send, open, close } = useWebSocket(
+  "wss://devproxy.staracademyapp.com/_t-ws-demo"
+);
 const sendMessage = () => {
-  send(JSON.stringify({ message: random({ exactly: 2, join: '-' }), ts: new Date().toISOString() }))
-}
+  send(
+    JSON.stringify({
+      message: random({ exactly: 2, join: "-" }),
+      ts: new Date().toISOString(),
+    })
+  );
+};
 const closeConnection = () => {
   close();
-}
+};
 const openConnection = () => {
   open();
-}
+};
+const forceError = () => {
+  throw new Error("testing sentry through a proxy");
+};
 </script>
 
 <template>
   <main>
-    <button class="p-4 bg-gray-100 m-2 hover:bg-gray-300" @click="openConnection">open</button>
-    <button class="p-4 bg-gray-100 m-2 hover:bg-gray-300" @click="closeConnection">close</button>
-    <button class="p-4 bg-gray-100 m-2 hover:bg-gray-300" @click="sendMessage">send</button>
-    <p class="break-normal">
-      Status: {{ status }}
-    </p>
-    <p class="break-normal">
-      Data: {{ data }}
-    </p>
+    <button
+      class="p-4 bg-gray-100 m-2 hover:bg-gray-300"
+      @click="openConnection"
+    >
+      open
+    </button>
+    <button
+      class="p-4 bg-gray-100 m-2 hover:bg-gray-300"
+      @click="closeConnection"
+    >
+      close
+    </button>
+    <button class="p-4 bg-gray-100 m-2 hover:bg-gray-300" @click="sendMessage">
+      send
+    </button>
+    <button class="p-4 bg-gray-100 m-2 hover:bg-gray-300" @click="forceError">
+      Force Error
+    </button>
+    <p class="break-normal">Status: {{ status }}</p>
+    <p class="break-normal">Data: {{ data }}</p>
   </main>
 </template>
